2f974d75432d2643686309aaa59b1c4e3e9eb227
Marcus Ellested <Marcusellested02@gmail.com>
2022-07-24 23:36:31 +0200

    First part of the documentation tutorial

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,14 +6,8 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        Welcome to animaldle - Wordle for animals!
       </p>
       <Counter start={3} />
     </div>
